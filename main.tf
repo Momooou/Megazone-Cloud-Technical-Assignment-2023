14dbc16ec59797bff819b466d4efe2c0392ea2df
Morris <wainamchan21@gmail.com>
2023-10-17 01:29:44 +0800

    Set up

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-east-1"
+  profile = "morrischan"
+}
