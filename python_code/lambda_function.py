e2dba3035b7fa77c0b565743cd0c725f1fe4cda6
Morris <wainamchan21@gmail.com>
2023-10-19 00:39:50 +0800

    test
    test

--- python_code/lambda_function.py
+++ python_code/lambda_function.py
@@ -1,14 +1,34 @@
 import json
+import random
+import string
+
 
 print("Loading function")
 
+url_maps = {
+    "longURL": "shortURL"
+}
+
+
+def generate_random_string():
+    letters_and_digits = string.ascii_lowercase + string.digits
+    random_string = ''.join(random.choices(letters_and_digits, k=5))
+    return random_string
+
+
 def url_shortener(event, context):
-    print('event:', event)
-    x = event['queryStringParameters']
-    print(x)
+    print('eventtttt:', event)
+    long_url = event['queryStringParameters']
+    short_url = generate_random_string()
+
+    if short_url in url_maps.values():
+        short_url = generate_random_string()
+
+    url_maps[long_url] = short_url
+
+    print(url_maps)
 
     return {
         'statusCode': 200,
         'body': json.dumps('Helloooo from Lambda!')
     }
-
