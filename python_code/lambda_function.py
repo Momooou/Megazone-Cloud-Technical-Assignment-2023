7058db6aa451931cf6c081998624f7bfa93de60d
Morris <wainamchan21@gmail.com>
2023-10-20 00:07:31 +0800

    test expire

--- python_code/lambda_function.py
+++ python_code/lambda_function.py
@@ -3,14 +3,26 @@ import random
 import string
 import base64
 import urllib.parse
+import time
 
 print("Loading function")
 
 url_maps = {
-    "shortURL": "longURL"
+    "shortURL": {"long_url": "longURL",
+                 'created_at': "time"}
 }
 
+def set_with_ttl(key, value, ttl):
+    url_maps[key] = {'long_url': value, 'created_at': time.time()}
 
+def get_with_ttl(key):
+    data = url_maps.get(key)
+    
+    if data and (time.time() - data['created_at']) < 30:
+        return data['long_url']
+    else:
+        return None
+    
 def generate_random_string():
     letters_and_digits = string.ascii_lowercase + string.digits
     random_string = ''.join(random.choices(letters_and_digits, k=5))
@@ -27,7 +39,10 @@ def url_shortener(event, context):
         while short_url in url_maps:
             short_url = generate_random_string()
 
-        url_maps[short_url] = long_url
+        # url_maps[short_url]['long_url'] = long_url
+        set_with_ttl(short_url, long_url, )
+
+        print(url_maps)
 
         return {
             'statusCode': 200,
@@ -41,7 +56,7 @@ def url_shortener(event, context):
             return {
                 'statusCode': 302,
                 'headers': {
-                    'Location': urllib.parse.unquote(url_maps[req_url])
+                    'Location': urllib.parse.unquote(get_with_ttl(req_url))
                 }
             }
         else:
